81d8058ba689c6a2c77d7dc34362887c0476ab68
jibinshan <jibinshanu23@gmail.com>
2024-02-24 14:08:57 +0530

    changed url of generateqr

--- client/src/Adminpage/GenerateQr.jsx
+++ client/src/Adminpage/GenerateQr.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 function GenerateQr() {
     const getqrapi = "https://angelsqr-3.onrender.com/qrcodes/"
-    const postqrapi = "https://angelsqr-3.onrender.com/generateQr"
+    const postqrapi = "https://angelsqr.vercel.app/login/qrcodes/generateQr"
     const [qr,setQr] = useState([])
     const [number,setNumber] = useState('')
     const fetchdata = async()=>{
