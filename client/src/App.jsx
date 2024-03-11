cae9344987bf90a97ff06317f6a4131e552e13b4
jibinshan <jibinshanu23@gmail.com>
2024-03-11 15:39:45 +0530

    added current change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,6 @@ function App() {
   return (
    <div>
      <Routes>
-<<<<<<< HEAD
         <Route path='/bio/:qrid' element={<Bio/>}/>
         <Route path='/photos/:qrid' element={<Photos/>}/>
         <Route path='/videos/:qrid' element={<Videos/>}/>
@@ -33,14 +32,6 @@ function App() {
         <Route path='/createuser/:qrid' element={<CreateUser/>}/>
         <Route path='/createtribute' element={<Createtribute/>}/>
         <Route path='/scanqr/:qrId' element={<Scanqr/>}/>
-=======
-        <Route path='/createuser' element={<CreateUser/>}/>
-        <Route path='/bio' element={<Bio/>}/>
-        <Route path='/photos' element={<Photos/>}/>
-        <Route path='/videos' element={<Videos/>}/>
-        <Route path='/tributes' element={<Tributes/>}/>
-        <Route path='/details' element={<Details/>}/>
->>>>>>> 42595a3845b6fcf906660a75e90ff4e62b94532e
         <Route path='/login' element={<Login/>}/>
      <Route element={<ProtectedRoute/>}>
         <Route path='/' element={<Dashboard/>}/>
